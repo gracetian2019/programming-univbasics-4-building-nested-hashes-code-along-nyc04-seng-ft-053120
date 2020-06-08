33332b8b42c447a8848b14064316dfd9d302480f
Grace Tian <gracetian4ever@gmail.com>
2020-06-08 03:42:36 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,27 +2,35 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name, :age
         },
       :matriarch => {
         #your key/value pairs here
+        :name, :age
         },
       :hero => {
         #your key/value pairs here
+        :name, :age, :status
         },
-      :hero_friends => []
+      :hero_friends => [{:name, :age, :attitude},
+      {:name, :age, :attitude}]
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name, :age
         },
       :matriarch => {
         #your key/value pairs here
+        :name, :age
         },
       :heroine => {
         #your key/value pairs here
+        :name, :age, :status
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {:name, :age, :attitude},
+        {:name, :age, :attitude}]
    }
   }
 
