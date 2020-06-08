4d9c29f305d9a50866e970a2dfb0fafdd3d0bfcc
Grace Tian <gracetian4ever@gmail.com>
2020-06-08 03:46:23 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -21,7 +21,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name, :age
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
         #your key/value pairs here
